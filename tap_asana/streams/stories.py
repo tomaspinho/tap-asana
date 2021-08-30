ac709bb4f38606c8cb4b7462c0475faf597c5be5
Tomás Pinho <me@tomaspinho.com>
2021-08-30 20:02:52 +0100

    separate syncing parents for stories so we don't run in pagination token expirations

--- tap_asana/streams/stories.py
+++ tap_asana/streams/stories.py
@@ -2,7 +2,9 @@
 from singer import utils
 from tap_asana.context import Context
 from tap_asana.streams.base import Stream
+import singer
 
+LOGGER = singer.get_logger()
 
 class Stories(Stream):
   name = "stories"
@@ -59,14 +61,40 @@ class Stories(Stream):
     bookmark = self.get_bookmark()
     session_bookmark = bookmark
     opt_fields = ",".join(self.fields)
-    for workspace in self.call_api("workspaces"):
-      for project in self.call_api("projects", workspace=workspace["gid"]):
-        for task in self.call_api("tasks", project=project["gid"]): 
-          task_gid = task.get('gid')
-          for story in Context.asana.client.stories.get_stories_for_task(task_gid=task_gid, opt_fields=opt_fields):
-            session_bookmark = self.get_updated_session_bookmark(session_bookmark, story[self.replication_key])
-            if self.is_bookmark_old(story[self.replication_key]):
-              yield story
+
+    LOGGER.info("Syncing stream stories: getting workspaces")
+    workspaces = [w for w in self.call_api("workspaces")]
+
+    LOGGER.info("Syncing stream stories: getting projects")
+    projects = [p for workspace in workspaces for p in self.call_api("projects", workspace=workspace["gid"])]
+    projects_len = len(projects)
+
+    LOGGER.info("Syncing stream stories: getting tasks")
+    tasks = []
+
+    for i, project in enumerate(projects):
+      Context.asana.maybe_refresh_access_token()
+
+      project_gid = project['gid']
+      num_tasks = Context.asana.client.projects.get_task_counts_for_project(project_gid, opt_pretty=True, opt_fields="num_tasks")["num_tasks"]
+
+      LOGGER.info("Syncing stream stories: getting %d tasks for project %s, %d/%d", num_tasks, project_gid, i+1, projects_len)
+
+      for t in self.call_api("tasks", project=project["gid"]):
+        tasks.append(t)
+
+    LOGGER.info("Syncing stream stories: getting tasks's stories")
+    tasks_len = len(tasks)
+    for i, task in enumerate(tasks):
+      Context.asana.maybe_refresh_access_token()
+      task_gid = task.get('gid')
+
+      LOGGER.info("Syncing stream stories: getting stories for task %s, %d/%d tasks", task_gid, i+1, tasks_len)
+
+      for story in Context.asana.client.stories.get_stories_for_task(task_gid=task_gid, opt_fields=opt_fields):
+        session_bookmark = self.get_updated_session_bookmark(session_bookmark, story[self.replication_key])
+        if self.is_bookmark_old(story[self.replication_key]):
+          yield story
     self.update_bookmark(session_bookmark)
 
 
